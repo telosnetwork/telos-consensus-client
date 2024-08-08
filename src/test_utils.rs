3a22f692a5b198e1cef8c8275eb30e01eac745dc
Guillermo Rodriguez <guillermo@telos.net>
2024-08-07 23:05:48 -0300

    Add simple fork test using leap-mock

---
+++ src/test_utils.rs
@@ -0,0 +1,100 @@
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+/*
+ * leap-mock client
+ */
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SetNumParams {
+    pub num: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SetJumpsParams {
+    pub jumps: Vec<(u32, u32)>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SetBlockInfo {
+    pub blocks: Vec<String>,
+    pub index: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MockResponse<T> {
+    pub result: Option<T>,
+    pub error: Option<String>,
+}
+
+#[derive(Error, Debug)]
+pub enum LeapMockError {
+    #[error("HTTP error: {0}")]
+    HttpError(#[from] reqwest::Error),
+    #[error("API error: {0}")]
+    ApiError(String),
+}
+
+#[derive(Debug)]
+pub struct LeapMockClient {
+    base_url: String,
+    client: Client,
+}
+
+impl LeapMockClient {
+    pub fn new(base_url: &str) -> Self {
+        LeapMockClient {
+            base_url: base_url.to_string(),
+            client: Client::new(),
+        }
+    }
+
+    pub async fn set_block(&self, params: SetNumParams) -> Result<String, LeapMockError> {
+        let url = format!("{}/set_block", self.base_url);
+
+        let resp = self.client.post(&url)
+            .json(&params)
+            .send()
+            .await?
+            .json::<MockResponse<String>>()
+            .await?;
+
+        match resp.result {
+            Some(result) => Ok(result),
+            None => Err(LeapMockError::ApiError(resp.error.unwrap_or_else(|| "Unknown error".to_string()))),
+        }
+    }
+
+    pub async fn set_jumps(&self, params: SetJumpsParams) -> Result<String, LeapMockError> {
+        let url = format!("{}/set_jumps", self.base_url);
+
+        let resp = self.client.post(&url)
+            .json(&params)
+            .send()
+            .await?
+            .json::<MockResponse<String>>()
+            .await?;
+
+        match resp.result {
+            Some(result) => Ok(result),
+            None => Err(LeapMockError::ApiError(resp.error.unwrap_or_else(|| "Unknown error".to_string()))),
+        }
+    }
+
+    pub async fn set_block_info(&self, params: SetBlockInfo) -> Result<String, LeapMockError> {
+        let url = format!("{}/set_block_info", self.base_url);
+
+        let resp = self.client.post(&url)
+            .json(&params)
+            .send()
+            .await?
+            .json::<MockResponse<String>>()
+            .await?;
+
+        match resp.result {
+            Some(result) => Ok(result),
+            None => Err(LeapMockError::ApiError(resp.error.unwrap_or_else(|| "Unknown error".to_string()))),
+        }
+    }
+}
+
---
+++ tests/mock_fork.rs
@@ -0,0 +1,74 @@
+use testcontainers::core::WaitFor;
+use testcontainers::{runners::AsyncRunner, ContainerAsync, GenericImage};
+use testcontainers::core::ContainerPort::Tcp;
+
+use tokio::sync::mpsc;
+
+use tracing::info;
+
+use alloy::hex;
+use alloy::primitives::FixedBytes;
+
+use telos_translator_rs::block::Block;
+use telos_translator_rs::translator::{Translator, TranslatorConfig};
+use telos_translator_rs::types::env::TESTNET_GENESIS_CONFIG;
+use telos_translator_rs::test_utils::{LeapMockClient, SetJumpsParams};
+
+
+#[tokio::test]
+async fn mock_fork() {
+
+    tracing_subscriber::fmt::init();
+
+    let control_port = 6970;
+    let chain_http_port = 8889;
+    let chain_ship_port = 18998;
+
+    let container: ContainerAsync<GenericImage> = GenericImage::new(
+        "guilledk/leap-mock",
+        "0.2.1@sha256:432c2b03c7c89e6ff963e237e7f9e2a184d770c79e41ed33e1b0a18f421047df",
+    )
+    .with_exposed_port(Tcp(chain_http_port))
+    .with_exposed_port(Tcp(chain_ship_port))
+    .with_exposed_port(Tcp(control_port))
+    .with_wait_for(WaitFor::message_on_stdout("Control server running on"))
+    .start()
+    .await
+    .unwrap();
+
+    let cntr_control_port = container.get_host_port_ipv4(control_port).await.unwrap();
+    let cntr_http_port = container.get_host_port_ipv4(chain_http_port).await.unwrap();
+    let cntr_ship_port = container.get_host_port_ipv4(chain_ship_port).await.unwrap();
+
+    // configure a fork from block 30 to 25
+    // currently running this with the fork on will cause translator to stall
+    // disable the mock_client related code to see translator reach end of fake chain
+    let mock_client = LeapMockClient::new(&format!("http://localhost:{cntr_control_port}"));
+
+    mock_client.set_jumps(SetJumpsParams{
+        jumps: vec![(30, 25)]
+    }).await.unwrap();
+
+    let config = TranslatorConfig {
+        http_endpoint: format!("http://localhost:{cntr_http_port}",),
+        ship_endpoint: format!("ws://localhost:{cntr_ship_port}",),
+        validate_hash: None,
+        start_block: 1,
+        stop_block: Some(99),
+        block_delta: 0,
+        ..TESTNET_GENESIS_CONFIG.clone()
+    };
+
+    let (tx, mut rx) = mpsc::channel::<(FixedBytes<32>, Block)>(1000);
+
+    let mut translator = Translator::new(config);
+    match translator.launch(Some(tx)).await {
+        Ok(_) => info!("Translator launched successfully"),
+        Err(e) => panic!("Failed to launch translator: {:?}", e),
+    }
+
+    while let Some((block_hash, block)) = rx.recv().await {
+        info!("{}:{}", block.block_num, hex::encode(block_hash));
+    }
+}
+
